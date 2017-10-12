4adb21386cd1856cec2b569d9d6bb1ea984cfc22
Hugh Guiney <hugh@3playmedia.com>
2017-10-12 18:35:20 -0400

    Evil ideas

---
+++ whys-poignant-guide/chapter-4/06-code-words.rb
@@ -0,0 +1,10 @@
+CODE_WORDS = {
+  'starmonkeys' => 'Phil and Pete, those prickly chancellors of the New Reich',
+  'catapult' => 'chucky go-go', 'firebomb' => 'Heat-Assisted Living',
+  'Nigeria' => "Ny and Jerry's Dry Cleaning (with Donuts)",
+  'Put the kabosh on' => 'Put the cable box on'
+}
+
+puts CODE_WORDS['catapult']
+
+puts CODE_WORDS.[]( 'catapult' )
