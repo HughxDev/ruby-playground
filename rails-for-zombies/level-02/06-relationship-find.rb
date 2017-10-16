173bf2debc1791b7127864f19213bd6c50147e6c
Hugh Guiney <hugh@3playmedia.com>
2017-10-16 18:03:29 -0400

    Rails for Zombies: Rails Init

--- rails-for-zombies/level-02/06-relationship-find.rb
+++ rails-for-zombies/level-02/06-relationship-find.rb
@@ -15,8 +15,10 @@
 
 ashley = Zombie.where(:name => "Ashley")
 # #<ActiveRecord::Relation [#<Zombie id: 1, name: "Ashley", graveyard: "Glen Haven Memorial Cemetery">]>
+
 ashley.weapons
 # Found an ActiveRecord::Relation instead of a Zombie object. Cannot find weapons on an ActiveRecord::Relation.
+
 ashley[0].weapons
 # #<ActiveRecord::Associations::CollectionProxy [#<Weapon id: 1, name: "Hammer", strength: 1, zombie_id: 1>]>
 # Successfully found all of Ashley's weapons.
