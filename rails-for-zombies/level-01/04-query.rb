74a6715346bbce58c4663d79d5d28c5e8d346b45
Hugh Guiney <hugh@3playmedia.com>
2017-10-16 10:50:11 -0400

    Rails for Zombies Level 1 complete

--- rails-for-zombies/level-01/04-query.rb
+++ rails-for-zombies/level-01/04-query.rb
@@ -1,4 +1,6 @@
 # Find all `Zombies` ordered by their names.
 
+all_zombies = Zombie.all.order(:name)
+
 # #<ActiveRecord::Relation [#<Zombie id: 2, name: "Ashley", graveyard: "Glen Haven Memorial Cemetery">, #<Zombie id: 1, name: "Bob", graveyard: "Chapel Hill Cemetery">, #<Zombie id: 3, name: "Katie", graveyard: "My Father's Basement">]>
 # Found all Zombies ordered by their names.
---
+++ rails-for-zombies/level-01/05-update.rb
@@ -0,0 +1,5 @@
+# Update `Zombie 3`'s graveyard to `'Benny Hills Memorial'`
+
+Zombie.find(3).update(:graveyard => 'Benny Hills Memorial')
+# true
+# Successfully updated Zombie 3's graveyard
